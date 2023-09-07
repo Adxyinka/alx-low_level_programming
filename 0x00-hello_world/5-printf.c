f86c6019f0c9ae5d827030e9f583970ed6ea3468
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-07 04:23:17 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints with printf function
+ * Return: returns 0 to indicate success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
