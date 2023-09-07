61a81ce3aa77c5043c7cf07a8850ed81b6dba5a8
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-07 09:46:44 -0700

    size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,5 @@ int main(void)
 	printf("Size of a long int: %c byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a flat: %f byte(s)\n", sizeof(f));
-	
 	return (0);
 }
