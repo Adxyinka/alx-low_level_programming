3eea7029b875ba59ef3e509370f1d306c4a8b039
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-08 16:47:16 -0700

    double digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combinations of two digits
+ * Return: 0 indicates success
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = a; c <= 9; c++)
+			{
+				for (d = (c == a ? b + 1 : 0); d <= 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+
+					if (a != 9 || b != 8 || c != 9 || d != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
