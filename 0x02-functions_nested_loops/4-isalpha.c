7d66e76b533d4d11e89d3f6f2ca1aeb555e8c033
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-12 17:09:38 -0700

    check lowercas and uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isalphia - a program that checks for lower case character
+ * _isalpha - a program that checks for lower case character
  * @c: is the character to be checked
  * Return: 1 if c is lower case and 0 otherwise
  */
 int _isalpha(int c)
 {
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-                return (1);
-        else
-                return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
