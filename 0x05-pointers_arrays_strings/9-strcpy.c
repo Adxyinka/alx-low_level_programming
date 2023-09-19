889b4af09047eebf6e2b53d21fed9f2831e0cccd
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-18 21:41:37 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - a program that copies a string
+ * @dest: destination value
+ * @src: source value
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j++] = '\0';
+	return (dest);
+}
