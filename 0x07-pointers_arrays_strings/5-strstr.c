a53ceca7f50cf6ffb5948295bc55907b1e982251
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-27 15:43:30 -0700

    str str

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - a program that finds the first occurence of substring
+ * @haystack: string to search in
+ * @needle: substring to search for
+ * return: pointer to the beginning or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int c, d;
+
+	if (*needle == 0)
+		return (haystak);
+
+	for (i = 0; haystack[c] != '\0'; c++)
+	{
+		for (d = 0; needle[d] != '\0'; d++)
+		{
+			if (haystack[c + d] != needle[d])
+				break;
+		}
+		if (needle[d] == '\0')
+			return (haystack +c);
+	}
+
+	return (NULL);
+}
