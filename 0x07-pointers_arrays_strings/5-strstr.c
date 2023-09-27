ad22354fa4b2e9738d3fd4a4d1c0a3900733c145
adxyinka <adeyinkaryusuf@gmail.com>
2023-09-27 15:46:20 -0700

    str str

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	if (*needle == 0)
 		return (haystak);
 
-	for (i = 0; haystack[c] != '\0'; c++)
+	for (c = 0; haystack[c] != '\0'; c++)
 	{
 		for (d = 0; needle[d] != '\0'; d++)
 		{
